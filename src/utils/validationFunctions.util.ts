2d13536e8ccf9e2481325fc94bdd112c9a488344
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-19 16:35:22 -0300

    feat: validation functions

---
+++ src/utils/validationFunctions.util.ts
@@ -0,0 +1,11 @@
+export const isValidEmail = (email: string) =>
+  // eslint-disable-next-line no-useless-escape
+  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+export const isValidName = (name: string) => name.length >= 4;
+export const isValidPassword = (password: string) =>
+  // eslint-disable-next-line no-useless-escape
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/.test(
+    password
+  );
