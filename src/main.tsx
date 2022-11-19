888d32fe6a10476d5bf576924b3264f0bb9ef69c
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-19 16:33:44 -0300

    feat: config react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import GlobalStyle from "./global/styles";
 import theme from "./global/theme";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root") as HTMLElement).render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <App />
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>
 );
