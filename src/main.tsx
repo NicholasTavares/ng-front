83ac617a2a17531ddc8e1c0635072ec2771f5c1c
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-19 17:05:25 -0300

    feat: add auth provider as context

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components";
+import { AuthProvider } from "./context/AuthContext";
 import App from "./App";
 import GlobalStyle from "./global/styles";
 import theme from "./global/theme";
@@ -8,10 +9,12 @@ import theme from "./global/theme";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root") as HTMLElement).render(
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
-  </QueryClientProvider>
+  <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </AuthProvider>
 );
