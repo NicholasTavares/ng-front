f05061b282de0de56686ea9b932158f441249465
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-19 17:12:37 -0300

    refactor: is invalid name function

--- src/utils/validationFunctions.util.ts
+++ src/utils/validationFunctions.util.ts
@@ -3,7 +3,7 @@ export const isValidEmail = (email: string) =>
   /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
     email
   );
-export const isValidName = (name: string) => name.length >= 4;
+export const isValidName = (name: string) => name.length >= 3;
 export const isValidPassword = (password: string) =>
   // eslint-disable-next-line no-useless-escape
   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/.test(
