b4f1bcb502abbf7e8de5330a466feeda648a4bb9
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-19 16:35:37 -0300

    feat: helper messages

---
+++ src/helpers/errorsMessages.helper.ts
@@ -0,0 +1,6 @@
+export const INVALID_PASSWORD =
+  "Your password should contain at least 6 characters, 1 lower and upper letter, 1 number, 1 special character";
+export const INVALID_EMAIL = "Invalid email";
+export const INVALID_NAME = "Name must have at minimum 4 characters";
+export const EMAIL_EXISTS = "Email already exists";
+export const INVALID_SIGNIN = "Incorrect email or password";
