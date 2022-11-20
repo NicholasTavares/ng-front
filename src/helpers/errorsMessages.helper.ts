38e0f3ff1381cdfbda0d7758431aabaec2c95d83
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-20 16:30:17 -0300

    refactor: error message

--- src/helpers/errorsMessages.helper.ts
+++ src/helpers/errorsMessages.helper.ts
@@ -1,6 +1,5 @@
 export const INVALID_PASSWORD =
   "Your password should contain at least 6 characters, 1 lower and upper letter, 1 number, 1 special character";
-export const INVALID_EMAIL = "Invalid email";
-export const INVALID_NAME = "Name must have at minimum 3 characters";
-export const EMAIL_EXISTS = "Email already exists";
+export const INVALID_USERNAME = "Name must have at minimum 3 characters";
+export const USERNAME_EXISTS = "Username already exists";
 export const INVALID_SIGNIN = "Incorrect email or password";
