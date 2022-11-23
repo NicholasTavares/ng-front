631db1617691000b7c9983807543a006b9573345
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-23 02:26:09 -0300

    feat: get all users api method

--- src/api/userAPI.ts
+++ src/api/userAPI.ts
@@ -8,6 +8,11 @@ type LoggedUserGetResponse = {
   };
 };
 
+type UsersGetProps = {
+  id: string;
+  username: string;
+};
+
 export const loggedUserGet = async (
   jwt?: string
 ): Promise<LoggedUserGetResponse> => {
@@ -18,6 +23,12 @@ export const loggedUserGet = async (
   });
   // convert cents to reais
   response.data.account.balance = response.data.account.balance / 100;
+
+  return response.data;
+};
+
+export const usersGet = async (): Promise<UsersGetProps[]> => {
+  const response = await baseAPI.get("/users/all");
 
   return response.data;
 };
