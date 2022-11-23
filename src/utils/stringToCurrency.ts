39d8943eece5eba32e22125ba96ef964cec54e61
NicholasTavares <nicholasbalby@hotmail.com>
2022-11-22 22:47:51 -0300

    feat: convert string to formated value function

---
+++ src/utils/stringToCurrency.ts
@@ -0,0 +1,7 @@
+export const stringToCurrenty = (value: string): string => {
+  let v = value.replace(/\D/g, "");
+  v = (+v / 100).toFixed(2) + "";
+  v = v.replace(".", ",");
+  v = v.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+  return v;
+};
